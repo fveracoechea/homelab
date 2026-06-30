568cbcbd95872a8ffc3b94928e2c498a8d4cfbb2
Francisco Veracoechea <veracoecheafrancisco@gmail.com>
2026-06-29 22:51:25 -0400

    update homelap settings

--- configuration.nix
+++ configuration.nix
@@ -4,6 +4,11 @@
     inputs.dotfiles.nixosModules.default
   ];
 
+  boot.loader.systemd-boot.enable = true;
+  boot.loader.efi.canTouchEfiVariables = true;
+
+  nixpkgs.system.allowUnfree = true;
+
   nix = {
     gc.automatic = true;
     optimise.automatic = true;
@@ -13,18 +18,58 @@
     settings.trusted-users = ["root" "@wheel"];
   };
 
+  dotfiles = {
+    docker.enable = true;
+    system-shell.enable = true;
+    timezone.enable = true;
+  };
+
   networking = {
     firewall.enable = true;
+    hostName = "homelab";
+    wireless.enable = true;
+    networkmanager.enable = true;
   };
+
+  programs.ssh.startAgent = true;
 
   services.openssh = {
     enable = true;
     openFirewall = true;
     settings = {
       PermitRootLogin = "no";
-      PasswordAuthentication = false;
+      # PasswordAuthentication = false;
       X11Forwarding = false;
     };
+  };
+
+  time.timeZone = "America/New_York";
+
+  i18n = let
+    locale = "en_US.UTF-8";
+  in {
+    defaultLocale = locale;
+    extraLocaleSettings = {
+      LC_ADDRESS = locale;
+      LC_IDENTIFICATION = locale;
+      LC_MEASUREMENT = locale;
+      LC_MONETARY = locale;
+      LC_NUMERIC = locale;
+      LC_NAME = locale;
+      LC_PAPER = locale;
+      LC_TELEPHONE = locale;
+      LC_MESSAGES = locale;
+      LC_TIME = locale;
+    };
+  };
+
+  services.xserver = {
+    enable = true;
+    xkb = {
+      layout = "us";
+      variant = "";
+    };
+    videoDrivers = ["modesetting"];
   };
 
   users.users.fveracoechea = {
@@ -36,4 +81,7 @@
       "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIB3gt/1eR57Gx6gI2PMyXcu0gMCq708ttjP54TLwR/fh fveracoechea@nixos"
     ];
   };
+
+  # DO NOT CHANGE
+  system.stateVersion = "26.05";
 }
--- home.nix
+++ home.nix
@@ -3,6 +3,12 @@
     inputs.dotfiles.homeManagerModules.default
   ];
 
+  dotfiles = {
+    shell.enable = true;
+    neovim.enable = true;
+    fonts.enable = true;
+  };
+
   home.username = "fveracoechea";
   home.homeDirectory = "/home/fveracoechea";
 
@@ -13,6 +19,6 @@
   };
 
   # DO NOT CHANGE
-  home.stateVersion = "24.05";
+  home.stateVersion = "26.05";
   programs.home-manager.enable = true;
 }
