b4517ee024e8d5068df15af79764bc29d3e9b009
Francisco Veracoechea <veracoecheafrancisco@gmail.com>
2026-07-02 16:41:49 -0400

    clean up

--- services/caddy.nix
+++ services/caddy.nix
@@ -1,10 +1,10 @@
-{config, ...}: {
+{...}: {
   services.caddy = {
     enable = true;
+
     virtualHosts."10.0.0.2".extraConfig = ''
       tls internal
       bind 10.0.0.2
-      reverse_proxy 127.0.0.1:${toString config.services.vaultwarden.config.ROCKET_PORT}
     '';
   };
 
