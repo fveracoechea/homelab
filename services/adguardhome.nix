824809d47a90e5218b4612949ca7cf2c807253fc
Francisco Veracoechea <veracoecheafrancisco@gmail.com>
2026-07-11 14:25:52 -0400

    rename adguard vhost to ad-blocker.veracoechea.com

--- services/adguardhome.nix
+++ services/adguardhome.nix
@@ -31,7 +31,7 @@
     };
   };
 
-  services.caddy.virtualHosts."addblock.veracoechea.com".extraConfig = ''
+  services.caddy.virtualHosts."ad-blocker.veracoechea.com".extraConfig = ''
     tls /var/lib/acme/veracoechea.com/fullchain.pem /var/lib/acme/veracoechea.com/key.pem
     reverse_proxy 127.0.0.1:8082
   '';
