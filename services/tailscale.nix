b6938df87112efe0270bcc5bc6820e6207477148
Francisco Veracoechea <veracoecheafrancisco@gmail.com>
2026-08-02 22:18:45 -0400

    tailscale: open firewall for direct WireGuard connections (UDP 41641)

--- services/tailscale.nix
+++ services/tailscale.nix
@@ -2,6 +2,9 @@
   services.tailscale = {
     enable = true;
     useRoutingFeatures = "server";
+    # Allow peers to initiate direct WireGuard connections (UDP 41641) instead of
+    # falling back to the DERP relay - critical for iPhones on the home LAN.
+    openFirewall = true;
     authKeyFile = "/var/lib/tailscale/auth-key";
     extraUpFlags = ["--login-server=https://vpn.veracoechea.com" "--accept-dns=false"];
     extraSetFlags = ["--advertise-routes=10.0.0.0/24" "--accept-dns=false"];
